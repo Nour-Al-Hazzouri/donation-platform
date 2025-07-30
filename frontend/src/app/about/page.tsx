182d12595c38258002694ffd98d90ff908a54a81
Nour-Al-Hazzouri <Nour4lhazzouri@gmail.com>
2025-07-30 14:49:45 +0200

    DP-6 Initial commit fo the about us page, re-using the modular components already created to render the initial code, and to be added later

---
+++ frontend/src/app/about/page.tsx
@@ -0,0 +1,15 @@
+import { Header } from '@/components/common/Header'
+import { Footer } from '@/components/common/Footer'
+import AboutUs from '@/components/home/aboutUs'
+
+export default function AboutPage() {
+  return (
+    <main className="min-h-screen flex flex-col">
+      <Header />
+      <div className="flex-grow">
+        <AboutUs />
+      </div>
+      <Footer />
+    </main>
+  )
+}
