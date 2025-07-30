e0886bebe9ac64f8c997deb3f9e31d07ac12203f
AymanAD <aymandandan24680@gmail.com>
2025-07-30 11:00:50 +0300

    DP-18 roles & permissions using spatie

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->nullable();
+            $table->string('avatar_url')->nullable();
+            $table->foreignId('location_id')->constrained('locations');
+            $table->boolean('is_verified')->default(false);
+            $table->enum('role', ['admin', 'moderator', 'user'])->default('user');
             $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
 
