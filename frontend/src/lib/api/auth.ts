8150b0146dbcca6641c78a83fb0513238d743298
AymanAD <aymandandan24680@gmail.com>
2025-08-18 11:52:41 +0300

    testing API_URL

--- frontend/src/lib/api/auth.ts
+++ frontend/src/lib/api/auth.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8000/api';
+const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const authApi = axios.create({
   baseURL: API_URL,
